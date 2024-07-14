1daefe45bc1a33b5007358108081026d7fd03833
Nilesh Gupta <nileshgupta1993@gmail.com>
2024-07-14 13:51:20 +0000

    data type in java script

---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version
+
+let name = "Nilesh";
+let age = 22;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// null => standalone value
+// undefined => 
+// symbol => use for find uniqueness 
+
+
+// object
+// array
+ 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
